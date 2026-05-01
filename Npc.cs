32bb14b5c7534bcf6b817ab9a71613f69e1faead
Declan <declynch95@gmail.com>
2026-05-02 00:22:46 +0100

    Made raycast work

--- Npc.cs
+++ Npc.cs
@@ -3,10 +3,21 @@ using System;
 
 public partial class Npc : CharacterBody2D
 {
-	public const float Speed = -50.0f;
+	private RayCast2D _rayCast;
+	public  float Speed = -50.0f;
 
 	public override void _PhysicsProcess(double delta){
 
+		_rayCast = GetNode<RayCast2D>("RayCast2D");
+
+		if (_rayCast.IsColliding())
+		{
+			Speed = 0.0f;
+		}
+		else
+		{
+			Speed = -50.0f;
+		}
 		Velocity = new Vector2(Speed,0);
 		MoveAndCollide(Velocity * (float)delta);
 	}
