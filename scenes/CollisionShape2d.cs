99f4b925472fa39ea9659cd047ac554e03224a17
Declan Lynch <declynch95@gmail.com>
2026-05-01 23:02:07 +0100

    Added Pay point

--- scenes/CollisionShape2d.cs
+++ scenes/CollisionShape2d.cs
@@ -5,16 +5,18 @@ public partial class CollisionShape2d : CollisionShape2D
 {
 	private Timer _timer;
 	private Timer _waitTimer;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 
 	{
-		_timer = GetNode<Timer>("Timer");
+		
+		_timer = GetNode<Timer>("../Timer");
 		_waitTimer = GetNode<Timer>("../WaitTimer");
 		_waitTimer.WaitTime = 1.0f;
 		_waitTimer.OneShot = false;
 		_waitTimer.Timeout += _on_wait_timer_timeout;
-
+		
 		Disabled = false;
 	}
 
@@ -37,4 +39,5 @@ public partial class CollisionShape2d : CollisionShape2D
 		_waitTimer.Stop();
 
 	}
+
 }
